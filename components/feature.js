e6cf6c78209c39424524e02ca36f1d8ac7fa07f7
Ivan <ivan.silva@multivarejogpa.com.br>
2019-07-01 21:50:14 -0300

    simple test

--- components/feature.js
+++ components/feature.js
@@ -7,7 +7,25 @@ export default class extends React.Component {
     return (
       <SbEditable content={content}>
         <div className="feature util__flex-eq">
-          <h2>{content.name}</h2>
+          <h2 className="teaser-head">{content.headline}</h2>
+          <img className="teaser-image" src={content.icon}/>
+          <p>{content.text}</p>
+          {console.log(content)}
+          <style jsx>{`
+            .teaser-head {color:red; 
+                font-size:32px
+              }
+              .teaser-image {
+                height:160px;
+              }
+              .teaser-text {
+                font-size:22;
+                color:black;
+                text-align: left;
+              }
+
+      
+          `}</style>
         </div>
       </SbEditable>
     )
--- components/index.js
+++ components/index.js
@@ -1,34 +1,21 @@
 import React from 'react'
-import Components from '../components/index'
-import Layout from '../components/layout'
-import StoryblokService from '../utils/storyblok-service'
+import Teaser from './teaser'
+import Feature from './feature'
+import Page from './page'
+import Grid from './grid'
 
-export default class extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      pageContent: props.page.data.story.content,
-    }
+const Components = {
+  'teaser': Teaser,
+  'feature': Feature,
+  'page': Page,
+  'grid': Grid
+}
+
+export default (blok) => {
+  if (typeof Components[blok.component] !== 'undefined') {
+    return React.createElement(Components[blok.component], { key: blok._uid, content: blok })
   }
-
-  static async getInitialProps({ query }) {
-    StoryblokService.setQuery(query)
-
-    return {
-      page: await StoryblokService.get('cdn/stories/home')
-    }
-  }
-
-  componentDidMount() {
-    StoryblokService.initEditor(this)
-  }
-
-  render() {
-    return (
-      <Layout settings={""}>
-        {/* We will define these settings later on */}
-        {Components(this.state.pageContent)}
-      </Layout>
-    )
-  }
+  return React.createElement(() => (
+    <div>The component {blok.component} has not been created yet.</div>
+  ), {key: blok._uid})
 }
